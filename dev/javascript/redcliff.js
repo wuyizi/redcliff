0d56d6d069cc5e8738896908fa3755733b983ff1
ben.luk <ben.luk@17230bbe-b06c-11dd-b6fe-af671ef85443>
2008-12-16 10:11:41 +0000

    remove alert message

--- dev/javascript/redcliff.js
+++ dev/javascript/redcliff.js
@@ -784,7 +784,6 @@
    if (navigator.appName == 'Microsoft Internet Explorer' &&
        navigator.appVersion.indexOf('MSIE 6.0')) {
       var sel = $('#select_tiles').get(0);
-      alert(sel.length);
       for (var i = sel.length - 1; i >= 0; i--) {
         if (sel.options[i].value > 0.01 && sel.options[i].value < 0.99) {
           sel.remove(i);
