702f3136728660d556c0ffe75ce61a0a8008e658
ben.luk <ben.luk@17230bbe-b06c-11dd-b6fe-af671ef85443>
2008-12-17 09:46:59 +0000

    Switch to 1.2 version of tiles

--- dev/javascript/redcliff.js
+++ dev/javascript/redcliff.js
@@ -73,7 +73,7 @@
     event_url: BASE + 'data/event.json?bpc=5',
     big_event_url: BASE + 'data/big_event.json?bpc=9',
     people_url: BASE +'data/people.json?bpc=7',
-    tile_url: 'http://mt.google.cn/mt?v=cnsg1.1&hl=zh-CN&x={X}&y={Y}&z={Z}'
+    tile_url: 'http://mt.google.cn/mt?v=cnsg1.2&hl=zh-CN&x={X}&y={Y}&z={Z}'
   };
 
   var FLAGS = {
